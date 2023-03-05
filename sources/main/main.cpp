2bc76ca4c88ad678fcf83a0be6193b4c2520ca2e
Александр Федоров <fedorovas@int.adtspb.ru>
2023-03-05 19:06:56 +0300

    aboba, now i can summ two numbers

--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -1,24 +1,34 @@
 #include "crow.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
-std::string parsFun(std::string str)
+std::vector<std::string> parsFun(std::string str)
 {
+    std::vector<std::string> vecStr(2);
     std::string output;
-    int numBuf = 0;
-    str.erase(0,1);
+    bool flag = false;
+    int j = 0;
     for (int i = 0; i < str.size(); i++)
-    {
-        if(str[i] == '=')
+    {  
+        if (str[i] == '=')
         {
-            str.erase(numBuf, i-numBuf);
+            str.erase(0, i+1);
+            flag = true;
+            i = -1;
         }
         else if (str[i] == '&')
         {
-            numBuf = i;
+            str.erase(0, i+1);
+            j += 1;
+            flag = false;
+        }
+        else if (flag == true)
+        {
+            vecStr[j].push_back(str[i]);
         }
     }
-    return str;
+    return vecStr;
 }
 
 int
@@ -31,8 +41,17 @@ main()
         return page;
     });
 
-    CROW_ROUTE(app, "/sum/<string>")([](std::string str){ 
-       return crow::response(parsFun(str));
-    });
+    CROW_ROUTE(app, "/sum")
+        .methods("POST"_method)(
+            [&](const crow::request& req)
+            {
+                std::vector<int> num(2);
+                num[0] = stoi(parsFun(req.body)[0]);
+                num[1] = stoi(parsFun(req.body)[1]);
+                std::cout << "sus\\n";
+                return std::to_string(num[0]+num[1]);
+            });
+
+    
     app.port(18080).multithreaded().run();
 }
